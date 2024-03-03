04b549ec6097b5536f57f9529f2d141a49397a9a
AHMAD SYAHIRAN <syahiranahmad@gmail.com>
2024-03-04 01:19:15 +0800

    delete

--- src/components/buttons/LoginButton.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-    import { Button } from '@/components/ui/button'
-</script>
-
-<template>
-    <router-link to="/login"><Button>Login</Button></router-link>
-</template>
